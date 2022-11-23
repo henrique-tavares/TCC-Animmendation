e99f41f16d19a94c18162dbd257f2896f7d17d3e
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2022-11-22 21:51:47 -0300

    dataset_handler - add pdm

--- dataset_handler/.devcontainer/entrypoint.sh
+++ dataset_handler/.devcontainer/entrypoint.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -f "requirements.txt" ]; then
-  task setup
-else
-  packages=("kaggle" "flake8" "black")
-  pip install ${packages[@]}
-  
-  task freeze
+if ! type pdm > /dev/null 2>&1; then
+  curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python -
+  pdm completion fish > ~/.config/fish/completions/pdm.fish
+  pdm plugin add pdm-vscode
 fi
 
-task dataset
+if [[ -f "pdm.lock" && -f "pyproject.toml" ]]; then
+  pdm install
+fi
+
 sudo chmod 777 data/datasets
+
+chmod +x scripts/download_datasets.sh
+mkdir -p data/raw_datasets
+scripts/download_datasets.sh data/raw_datasets
+
+fish -c "fisher install pure-fish/pure"
 
 exec "$@"
